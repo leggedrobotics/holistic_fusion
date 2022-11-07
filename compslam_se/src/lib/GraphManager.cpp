bc0577c5628832fd3b5ea51f732a1b44efabcff3
Timo Schönegg <timoscho@ethz.ch>
2022-11-07 13:40:41 +0000

    GraphManager.cpp edited online with Bitbucket

--- compslam_se/src/lib/GraphManager.cpp
+++ compslam_se/src/lib/GraphManager.cpp
@@ -20,15 +20,15 @@ bool GraphManager::initImuIntegrators(const double g, const std::string& imuGrav
     throw std::runtime_error("Gravity direction must be either 'up' or 'down'.");
   }
   // Set noise and bias parameters
-  imuParamsPtr_->accelerometerCovariance = gtsam::Matrix33::Identity(3, 3) * graphConfigPtr_->accNoiseDensity;
-  imuParamsPtr_->biasAccCovariance = gtsam::Matrix33::Identity(3, 3) * graphConfigPtr_->accBiasRandomWalk;
-  imuParamsPtr_->gyroscopeCovariance = gtsam::Matrix33::Identity(3, 3) * graphConfigPtr_->gyroNoiseDensity;
-  imuParamsPtr_->biasOmegaCovariance = gtsam::Matrix33::Identity(3, 3) * graphConfigPtr_->gyroBiasRandomWalk;
+  imuParamsPtr_->accelerometerCovariance = gtsam::Matrix33::identity(3, 3) * graphConfigPtr_->accNoiseDensity;
+  imuParamsPtr_->biasAccCovariance = gtsam::Matrix33::identity(3, 3) * graphConfigPtr_->accBiasRandomWalk;
+  imuParamsPtr_->gyroscopeCovariance = gtsam::Matrix33::identity(3, 3) * graphConfigPtr_->gyroNoiseDensity;
+  imuParamsPtr_->biasOmegaCovariance = gtsam::Matrix33::identity(3, 3) * graphConfigPtr_->gyroBiasRandomWalk;
   imuParamsPtr_->integrationCovariance =
-      gtsam::Matrix33::Identity(3, 3) *
+      gtsam::Matrix33::identity(3, 3) *
       graphConfigPtr_->integrationNoiseDensity;  // error committed in integrating position from velocities
   imuParamsPtr_->biasAccOmegaInt =
-      gtsam::Matrix66::Identity(6, 6) * graphConfigPtr_->biasAccOmegaPreint;  // covariance of bias used for preintegration
+      gtsam::Matrix66::identity(6, 6) * graphConfigPtr_->biasAccOmegaPreint;  // covariance of bias used for preintegration
   gtsam::Vector3 accBiasPrior(graphConfigPtr_->accBiasPrior);
   // Use previously defined prior for gyro
   imuBiasPriorPtr_ = std::make_shared<gtsam::imuBias::ConstantBias>(graphConfigPtr_->accBiasPrior, graphConfigPtr_->gyroBiasPrior);
