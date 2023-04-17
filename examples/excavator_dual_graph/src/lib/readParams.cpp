bbeeb8ad738daee450ecf7cbd9789dd6bf7dfa80
Juli <nubertj@ethz.ch>
2023-04-17 12:53:18 +0200

    copyright notice

--- examples/excavator_dual_graph/src/lib/readParams.cpp
+++ examples/excavator_dual_graph/src/lib/readParams.cpp
@@ -1,3 +1,10 @@
+/*
+Copyright 2023 by Julian Nubert, Robotic Systems Lab, ETH Zurich.
+All rights reserved.
+This file is released under the "BSD-3-Clause License".
+Please see the LICENSE file that has been included as part of this package.
+ */
+
 #include "excavator_dual_graph/ExcavatorEstimator.h"
 
 namespace excavator_se {
